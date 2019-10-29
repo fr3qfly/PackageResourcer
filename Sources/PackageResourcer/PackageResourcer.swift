11b69597bf1558b157e7363403473be6d12ae8b4
Unknown <bszamody@gmail.com>
2019-10-29 19:52:12 +1100

    Interface implemented and tested

--- Sources/PackageResourcer/PackageResourcer.swift
+++ Sources/PackageResourcer/PackageResourcer.swift
@@ -1,3 +1,78 @@
-struct PackageResourcer {
-    var text = "Hello, World!"
+import Foundation
+
+typealias DownloadResult = Result<(Data, HTTPURLResponse), Error>
+
+class PackageResourcer {
+    
+    let assetURLs: [URL]
+    
+    var assetCompletion: ((Result<[Data], Error>) -> Void)
+    
+    var downloadResults: [DownloadResult] = [] {
+        didSet {
+            guard downloadResults.count == assetURLs.count else {
+                return
+            }
+            assetCompletion(mapDownloadResults(downloadResults))
+        }
+    }
+    
+    public init(urls: [URL], completion: @escaping (Result<[Data], Error>) -> Void) {
+        self.assetURLs = urls
+        self.assetCompletion = completion
+        
+        assets()
+    }
+    
+    public func assets() {
+        assetURLs.forEach { (url) in
+            getAsset(url) {[weak self] (result) in
+                self?.downloadResults.append(result)
+            }
+        }
+        
+    }
+    
+    func getAsset(_ url: URL, completion: (DownloadResult) -> Void) {
+        //TODO: Implement
+        
+        let data = Data()
+        let response = HTTPURLResponse()
+        completion(.success((data, response)))
+    }
+    
+    func mapDownloadResults(_ results: [DownloadResult]) -> Result<[Data], Error> {
+        //TODO: implement
+        do {
+            let sorted = try results
+                .map { (result) -> Data in
+                    switch result {
+                    case .failure(let error):
+                        throw error
+                    case .success((let data, _)):
+                        return data
+                    }
+                }
+                .sorted { (first, second) -> Bool in
+                    first.count < second.count
+                }
+            return .success(sorted)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
+public extension URL {
+    static var mock: [URL] {
+        return [
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/forward-chevron%401x.png",
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/forward-chevron%402x.png",
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/forward-chevron%403x.png",
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/back-chevron%401x.png",
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/back-chevron%402x.png",
+            "https://raw.githubusercontent.com/BalazsSzamody/Eureka/master/Source/Resources/Eureka.bundle/back-chevron%403x.png",
+        ]
+        .compactMap({ URL(string: $0) })
+    }
 }
--- Tests/PackageResourcerTests/PackageResourcerTests.swift
+++ Tests/PackageResourcerTests/PackageResourcerTests.swift
@@ -2,11 +2,33 @@ import XCTest
 @testable import PackageResourcer
 
 final class PackageResourcerTests: XCTestCase {
+    
+    var resourcer: PackageResourcer!
+    
+    var exp: XCTestExpectation!
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PackageResourcer().text, "Hello, World!")
+        let urls = URL.mock
+        exp = expectation(description: "N/A")
+        var results: [Data] = []
+        resourcer = PackageResourcer(urls: urls, completion: { (result) in
+            switch result {
+            case .failure(let error):
+                XCTFail("Should be successfull, Error: \(error.localizedDescription)")
+            case .success(let data):
+                results = data
+            }
+            self.exp.fulfill()
+        })
+        wait(for: [exp], timeout: 30)
+        XCTAssertEqual(results.count, 6)
     }
 
     static var allTests = [
